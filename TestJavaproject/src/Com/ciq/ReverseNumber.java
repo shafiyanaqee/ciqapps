de6a8cdaaf0c660f864379ec61ced1249fee237e
w3schools-test <naqeeshafiya@gmail.com>
2025-03-15 13:00:50 +0530

    new changes

---
+++ TestJavaproject/src/Com/ciq/ReverseNumber.java
@@ -0,0 +1,20 @@
+package Com.ciq;
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int reversed = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        System.out.println("Reversed Number: " + reversed);
+        sc.close();
+    }
+}
